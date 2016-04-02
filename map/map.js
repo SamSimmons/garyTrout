799c162dc376b563a88f9dda66096cb0fbb1ecf8
sam <samuel.j.simmons@gmail.com>
2016-04-02 13:33:27 +1300

    add draw Marker function

--- map/map.js
+++ map/map.js
@@ -1,46 +1,42 @@
+//TODO refactor so all helper functions are in seperate modules
+//look into making it responsive.
+
 var width = 960,
     height = 960;
 
-var svg = d3.select("body").append("svg")
-  .attr("width", width)
-  .attr("height", height);
+var arr  = []
 
-  d3.json("lake.json", function(err, lake) {
-   	if (err) return console.error(error);
+var drawMarker = (trout, el) => {
+  el.append("circle")
+    .attr('class', 'marker-out')
+      .attr("cx", trout.x)
+        .attr("cy", trout.y)
+          .attr("r", 40)
 
-   	var projection = d3.geo.mercator()
-    	.center([176.58, -38.04])
-        .scale(550000)
-    
-    svg.append("path")
-      .datum(lake)
-      .attr("d", d3.geo.path().projection(projection));
-
-    var drawMarker = (trout) => {
-      svg.append("circle")
-        .attr('class', 'marker-out')
-          .attr("cx", trout.x)
-            .attr("cy", trout.y)
-              .attr("r", 40)
-
-      svg.append("circle")
-        .attr('class', 'marker')
-          .attr("cx", trout.x)
-            .attr("cy", trout.y)
-              .attr("r", 5);
-    }
-  var arr  = [{ x: 423, y: 399 }, { x: 493, y:526}, {x: 512, y:430}]
-
-  svg.on('click', function() {
-    var coords = d3.mouse(this);
-    console.log(coords);
-    arr.forEach( elt => drawMarker(elt))
-    // drawMarker(coords[0], coords[1]);
-  });
-});
+  el.append("circle")
+    .attr('class', 'marker')
+      .attr("cx", trout.x)
+        .attr("cy", trout.y)
+          .attr("r", 5);
+}
 
 //will need a function for adding individual trout to db
-var addTrout = () => {}
+var addTrout = (coords) => {
+  var newTrout = {
+    x: coords[0],
+    y: coords[1],
+    id: Date.now(),
+    angler: "",
+    time: "",
+    day: "",
+    month: "",
+    year: "",
+    lure: "",
+    comment: "" 
+  }
+  arr.push(newTrout)
+  return newTrout
+}
 
 //will need a function for fetching information on a selected trout 
 var fetchTrout = () => {}
@@ -50,3 +46,29 @@ var drawTrout = () => {}
 
 //will need a function for drawing all selected trout to the map
 var drawAllTrout = (arr) => {}
+
+
+//appends the map to the page
+var map = d3.select("body").append("svg")
+  .attr("width", width)
+  .attr("height", height)
+
+  d3.json("lake.json", function(err, lake) {
+    if (err) return console.error(error);
+
+    var projection = d3.geo.mercator()
+      .center([176.58, -38.04])
+        .scale(550000)
+    
+    map.append("path")
+      .datum(lake)
+      .attr("d", d3.geo.path().projection(projection))
+
+  map.on('click', function() {
+    var coords = d3.mouse(this)
+    var trout = addTrout(coords)
+    drawMarker(trout, map)
+  })
+
+
+})
