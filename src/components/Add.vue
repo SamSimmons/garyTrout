18cb1f648171022357595ba9ede005a699aad93e
sam <samuel.j.simmons@gmail.com>
2016-04-03 23:41:08 +1200

    init vue

---
+++ src/components/Add.vue
@@ -0,0 +1,15 @@
+<template>
+	<h1>ADD A</h1>
+	<h2>{{what}}</h2>
+</template>
+
+<script>
+	export default {
+		name: 'Add',
+		data: () => {
+			return {
+				what: 'A BIG FAT OLD TROUT'
+			}
+		}
+	}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="home-wrapper">
+		<h2>{{text}}</h2>
+		<h2>Above?</h2>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Home',
+		data: function() {
+			return {
+				text: 'Please be right?'
+			}
+		
+		}
+	}
+</script>
